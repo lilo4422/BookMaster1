4e855e4e6b0fa788e44e6215a758538ff6f4c091
Егор Рябцев <egor11212007@mail.ru>
2025-11-06 11:24:33 +0300

    test

--- BookMaster1/View/Windows/AddEditCustomerWindow.xaml.cs
+++ BookMaster1/View/Windows/AddEditCustomerWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,26 +24,71 @@ namespace BookMaster1.View.Windows
         public AddEditCustomer()
         {
             InitializeComponent();
-
             string lastIndex = App.context.Customer.Max(customer => customer.Id);
             int lastIndexNumber = Convert.ToInt32(lastIndex.Remove(0, 1));
-            IdTB.Text = $"C{++lastIndexNumber}";
+            lastIndexNumber++;
+            IdTB.Text = $"C{lastIndexNumber}";
+
+            EditBTN.Visibility = Visibility.Collapsed;
+            SaveBTN.Visibility = Visibility.Visible;
+            CitiesCmb.ItemsSource = App.context.City.ToList();
         }
         public AddEditCustomer(Customer selectedCustomer)
         {
             InitializeComponent();
 
+            EditBTN.Visibility = Visibility.Visible;
+            SaveBTN.Visibility = Visibility.Collapsed;
+   
             DataContext = selectedCustomer;
+
+            CitiesCmb.ItemsSource = App.context.City.ToList();
         }
 
         private void SaveBTN_Click(object sender, RoutedEventArgs e)
         {
+            if (string.IsNullOrEmpty(FNameTB.Text) ||
+                string.IsNullOrEmpty(LNameTB.Text)||
+                string.IsNullOrEmpty(AddressTB.Text) ||
+                string.IsNullOrEmpty(PhoneTB.Text) ||
+                string.IsNullOrEmpty(ZipTB.Text) ||
+                string.IsNullOrEmpty(EmailTB.Text) ||
+                CitiesCmb.SelectedItem == null)
+            {
+                MessageBox.Show("Заполните все поля", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                Customer newCustomer = new Customer()
+                {
+                    Id = IdTB.Text,
+                    Fname = FNameTB.Text,
+                    Lname = LNameTB.Text,
+                    MiddleName = MNameTB.Text,
+                    Zip = ZipTB.Text,
+                    Addres = AddressTB.Text,
+                    PhoneNubmer = PhoneTB.Text,
+                    Email = EmailTB.Text,
+                    CityId = Convert.ToInt32(CitiesCmb.SelectedValue)
+                };
+                App.context.Customer.Add(newCustomer);
+                App.context.SaveChanges();
+                MessageBox.Show("Сотрудник успешно создан", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
 
+            }
         }
 
         private void CancelBTN_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void EditBTN_Click(object sender, RoutedEventArgs e)
+        {
+            
+            App.context.SaveChanges();
+
+            MessageBox.Show("Изменения успешно сохранены", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
